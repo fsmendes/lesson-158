c4f78432ec37bdbc7e7e5b668fee7e811d61ef24
Fabio Mendes <fmendes@tsit.com.br>
2025-05-02 14:44:56 -0300

    Create CI script.

---
+++ my-app/upgrade.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+new_ver=$1
+
+echo "new version: $new_ver"
+
+# Simulate release of the new docker images
+docker tag nginx:1.27.5 cloudkoda/nginx:$new_ver
+
+# Push new version to dockerhub
+docker push cloudkoda/nginx:$new_ver
+
+# Create temporary folder
+tmp_dir=$(mktemp -d)
+echo $tmp_dir
+
+# Clone GitHub repo
+git clone git@github.com:fsmendes/lesson-158.git $tmp_dir
+
+# Update image tag
+sed -i '' -e "s/cloudkoda\/nginx:.*/cloudkoda\/nginx:$new_ver/g" $tmp_dir/my-app/1-deployment.yaml
+
+# Commit and push
+cd $tmp_dir
+git add .
+git commit -m "Update image to $new_ver"
+git push origin main
+cd ~/devel/lesson-158
+
+# Optionally on build agents - remove folder
+rm -rf $tmp_dir
